e5ab498257b1a3b47ff354bbdeaea7dca0696fe9
Jonah Smith <alexdee6@bigpond.com>
2024-06-22 16:59:02 +1000

    fix: missing import

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-// src/index.js
-var m = require("mithril")
+// Imports
+var m = require("mithril");
+var UserList = require("./views/UserList");
 
-var UserList = require("./views/UserList")
-var ComposerForm = require("./views/ComposerForm")
-var Layout = require("./views/Layout")
-
-m.mount(document.body, UserList)
-
-// m.route(document.body, "/list", 
-//     {
-//         "/list": {
-//             render: function() {
-//                 return m(Layout, m(UserList))
-//             }
-//         },
-//         "/edit/:id": {
-//             render: function(vnode) {
-//                 return m(Layout, m(UserForm, vnode.attrs))
-//             }
-//         },
-//     })
+m.mount(document.body, UserList);
