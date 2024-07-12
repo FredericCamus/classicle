49dec06741d02fe5a973c5081d67eac4e6c45a8a
Jonah Smith <alexdee6@bigpond.com>
2024-07-12 16:04:59 +1000

    fix: routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ var Game = require("./views/Game");
 var Stats = require("./views/Stats");
 var Layout = require("./views/Layout");
 
-// m.route.prefix = "";
+m.route.prefix = "";
 
-const path = window.location.search.slice(1) || "/";
+const path = window.location.pathname.slice(1) || "/";
 
-console.log("DEBUG", window.location.search.slice(1) || "/");
+console.log("DEBUG PATHNAME:", window.location.pathname);
 
-m.route(document.body, "/classicle", {
-    "/classicle": {
+m.route(document.body, path, {
+    "/": {
         render: function() {
             return m(Layout, m(Game));
         }
