5b157034d3bdb4f81fe4d02194fe75123c323419
Jonah Smith <alexdee6@bigpond.com>
2024-07-13 17:36:10 +1000

    fix: asset paths

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -2,9 +2,9 @@ var m = require("mithril")
 
 module.exports = {
     view: function(vnode) {
-        const about_icon = m("img.navicon", {src: "/assets/question.png", alt: "About"});
-        const classicle_icon = m("img.logo", {src: "/assets/logo.png", alt: "CLASSICLE"});
-        const stats_icon = m("img.navicon", {src: "/assets/stats.png", alt: "Statistics"});
+        const about_icon = m("img.navicon", {src: "./assets/question.png", alt: "About"});
+        const classicle_icon = m("img.logo", {src: "./assets/logo.png", alt: "CLASSICLE"});
+        const stats_icon = m("img.navicon", {src: "./assets/stats.png", alt: "Statistics"});
         return m("main.container1", [
             m("nav.menu", [
                 m(m.route.Link, {href: "/classicle"}, about_icon),
